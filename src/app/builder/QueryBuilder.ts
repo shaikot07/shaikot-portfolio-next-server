245853c46c7fac7d57a0109feeca5bddfbe54249
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-08 00:50:16 +0600

    create search method

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,1 +1,37 @@
-//  here i made queray bilder dynamic 
+import { FilterQuery, Query } from 'mongoose';
+
+class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  search(searchableFields: string[]) {
+    const searchTerm = this?.query?.searchTerm;
+    if (searchTerm) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableFields.map(
+          (field) =>
+            ({
+              [field]: { $regex: searchTerm, $options: 'i' },
+            }) as FilterQuery<T>,
+        ),
+      });
+    }
+
+    return this;
+  }
+
+  
+
+  
+
+ 
+
+ 
+}
+
+export default QueryBuilder;
