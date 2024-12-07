e458b6d301a032cee259c42fc10cf6185b84f4aa
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-08 00:24:35 +0600

    unhandledRejection uncaughtException  error handle

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
 
+
+
+let server: Server;
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
@@ -15,3 +19,20 @@ async function main() {
 }
 
 main();
+
+//  this is   unhandledRejection uncaughtException  error handle
+ 
+ process.on('unhandledRejection', () => {
+  console.log(`😈 unahandledRejection is detected , shutting down ...`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on('uncaughtException', () => {
+  console.log(`😈 uncaughtException is detected , shutting down ...`);
+  process.exit(1);
+});
