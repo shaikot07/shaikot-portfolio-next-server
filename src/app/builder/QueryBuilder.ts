7ed10c7e5c6022ae8027e68ece2d140bf84d50a6
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-08 01:02:30 +0600

    make pagenation method

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -50,7 +50,16 @@ filter() {
     return this;
   }
 
- 
+//   pagination method 
+  paginate() {
+    const page = Number(this?.query?.page) || 1;
+    const limit = Number(this?.query?.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+
+    return this;
+  }
 
  
 }
