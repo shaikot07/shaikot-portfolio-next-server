d8e81144b04e08217d75341721c16fa8eb53668f
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-08 01:08:51 +0600

    studentSearchableFields

---
+++ src/app/modules/student/student.constant.ts
@@ -0,0 +1,9 @@
+
+// j j filed gula dite search korbe user sei field gula amon kore dite hbe 
+
+export const studentSearchableFields = [
+    'email',
+    'name.firstName',
+    'presentAddress',
+  ];
+  
