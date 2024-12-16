d694ac1874f24bcd71ca9c35a1d25bf57ae9de78
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-16 23:32:36 +0600

    fix import problem for offered route in the  index.ts file

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -8,6 +8,7 @@ import { FacultyRoutes } from '../modules/Faculty/faculty.route';
 import { AdminRoutes } from '../modules/Admin/admin.route';
 import { CourseRoutes } from '../modules/Course/course.route';
 import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route';
+import { offeredCourseRoutes } from '../modules/OfferedCourse/OfferedCourse.route';
 
 const router = Router();
 
