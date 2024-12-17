3fc4423036995bf9f6f187a5e4b75014b2f37029
shaikot07 <saifulislamshaikat007@gmail.com>
2024-12-17 20:36:29 +0600

    make a password change route

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -13,12 +13,12 @@ router.post(
   AuthControllers.loginUser,
 );
 
-// router.post(
-//   '/change-password',
-//   auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
-//   validateRequest(AuthValidation.changePasswordValidationSchema),
-//   AuthControllers.changePassword,
-// );
+router.post(
+  '/change-password',
+  auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
+  validateRequest(AuthValidation.changePasswordValidationSchema),
+  AuthControllers.changePassword,
+);
 
 // router.post(
 //   '/refresh-token',
